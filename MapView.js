97d8724d3552da9d73e083977f9f880c1690d4ce
nahel-b <nahelbelma@gmail.com>
2024-08-17 01:33:41 +0200

    add custom cluster

--- MapView.js
+++ MapView.js
@@ -1,6 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { View, Text, Animated, Dimensions, Image, TouchableOpacity, ActivityIndicator,ScrollView } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import { Marker } from 'react-native-maps';
 import * as Location from 'expo-location';
 import { PanGestureHandler, GestureHandlerRootView, State } from 'react-native-gesture-handler';
 import Ionicons from 'react-native-vector-icons/Ionicons';
@@ -10,6 +10,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { Rating } from 'react-native-ratings'; // Importer le composant Rating
 import { useTheme } from './context/ThemeContext';
+
+import MapView from "react-native-map-clustering";
+
 
 import { addRestaurant } from './api'; 
 
@@ -106,262 +109,272 @@ const markersData = [
 ];
 
 const App = () => {
-  const mapRef = useRef(null);
-  const [selectedMarker, setSelectedMarker] = useState(null);
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [location, setLocation] = useState(null); // Localisation actuelle
-  const [loading, setLoading] = useState(false); // Indicateur de chargement de localisation
-  const [error, setError] = useState(null); // Erreur de localisation
-  const [storedLocation, setStoredLocation] = useState(null); // Dernière position enregistrée
-
-  const animatedHeight = useRef(new Animated.Value(0)).current;
-  const markerPressedRef = useRef(false);
-
-  // Charger la dernière position stockée dans AsyncStorage au démarrage
-  useEffect(() => {
-    const loadStoredLocation = async () => {
-
-        const newRestaurant = {
-            id: 1,
-            title: "Best Tacos",
-            description: "This is the description of Marker 1",
-            type: "restaurant de tacos",
-            coordinate: { latitude: 49.300, longitude: 2.641135858490601 },
-            image: "maison",
-            rating: 4.5,
-            reviews: [
-              {
-                name: "Jean Dupont",
-                dish: "Boeuf Bourguignon",
-                price: 25,
-                comment: "Le boeuf était parfaitement cuit, tendre et savoureux.",
-                rating: 4.5,
-                emoji: "🥩"
-              },
-              // Autres avis...
-            ],
-          };
-          
-          try {
-            const response = await addRestaurant(newRestaurant);
-            console.log('Restaurant added:', response);
-          } catch (error) {
-            console.error('Error adding restaurant:', error);
+    const mapRef = useRef(null);
+    const [selectedMarker, setSelectedMarker] = useState(null);
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const [location, setLocation] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const [storedLocation, setStoredLocation] = useState(null);
+    const { theme } = useTheme();
+    const animatedHeight = useRef(new Animated.Value(0)).current;
+    const markerPressedRef = useRef(false);
+  
+    useEffect(() => {
+      const loadStoredLocation = async () => {
+        try {
+          const value = await AsyncStorage.getItem(STORAGE_KEY);
+          if (value !== null) {
+            const savedLocation = JSON.parse(value);
+            setStoredLocation(savedLocation);
+          } else {
+            setStoredLocation(DEFAULT_LOCATION);
           }
-        
-      try {
-        const value = await AsyncStorage.getItem(STORAGE_KEY);
-        if (value !== null) {
-          const savedLocation = JSON.parse(value);
-          setStoredLocation(savedLocation);
-
-        } else {
-          setStoredLocation(DEFAULT_LOCATION); // Utiliser la position par défaut si aucune position n'est trouvée
+          handleLocationButtonPress();
+        } catch (e) {
+          console.log("Erreur lors du chargement de la position stockée", e);
         }
-        handleLocationButtonPress(); // Centrer la carte sur la position actuelle ou enregistrée
-      } catch (e) {
-        console.log("Erreur lors du chargement de la position stockée", e);
+      };
+      loadStoredLocation();
+    }, []);
+  
+    useEffect(() => {
+      if (storedLocation) {
+        centerMapOnLocation();
       }
-    };
-
-    loadStoredLocation();
-
-
-    
-
-
-
-}, []);
-
-useEffect(() => {
-    if (storedLocation) {
-        centerMapOnLocation(); // Centrer la carte dès que storedLocation est mis à jour
-    }
-}, [storedLocation]);
-
-  // Obtenir la position actuelle de l'utilisateur
-  const getCurrentLocation = async () => {
-    setLoading(true);
-    setError(null);
-    try {
+    }, [storedLocation]);
+  
+    const getCurrentLocation = async () => {
+      setLoading(true);
+      setError(null);
+      try {
         let { status } = await Location.requestForegroundPermissionsAsync();
         if (status !== 'granted') {
-            setError('Permission refusée');
-            setLoading(false);
-            return;
+          setError('Permission refusée');
+          setLoading(false);
+          return;
         }
-
+  
         let currentLocation = await Location.getCurrentPositionAsync({});
         setLocation(currentLocation);
-
-        // Stocker la position dans AsyncStorage
         await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(currentLocation.coords));
         setLoading(false);
-        console.log("Position actuelle", currentLocation);
-        setStoredLocation(currentLocation.coords); // Mettre à jour la dernière position connue
-
-        // Centrer la carte dès que la localisation est obtenue
+        setStoredLocation(currentLocation.coords);
         centerMapOnLocation();
-    } catch (err) {
+      } catch (err) {
         setError('Échec de localisation');
         console.log("Erreur lors de la localisation", err);
-    } finally {
+      } finally {
         setLoading(false);
-    }
-};
-
-  // Centrer la carte sur la position actuelle ou enregistrée
-  const centerMapOnLocation = () => {
-    if (mapRef.current && storedLocation) {
+      }
+    };
+  
+    const centerMapOnLocation = () => {
+      if (mapRef.current && storedLocation) {
+        mapRef.current.animateToRegion({
+          latitude: storedLocation.latitude,
+          longitude: storedLocation.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }, 1000);
+      }
+    };
+  
+    const handleLocationButtonPress = async () => {
+      await getCurrentLocation();
+      if (storedLocation) {
+        centerMapOnLocation();
+      }
+    };
+  
+    const handleMarkerPress = (marker) => {
+      markerPressedRef.current = true;
+      setSelectedMarker(marker);
+  
       mapRef.current.animateToRegion({
-        latitude: storedLocation.latitude,
-        longitude: storedLocation.longitude,
-        latitudeDelta: 0.01,
-        longitudeDelta: 0.01,
+        latitude: marker.coordinate.latitude - 0.0005,
+        longitude: marker.coordinate.longitude,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
       }, 1000);
-    }
-  };
-
-  // Gestion du clic sur le bouton de localisation
-  const handleLocationButtonPress = async () => {
-    await getCurrentLocation(); // Chercher la position actuelle
-    if (storedLocation) { // Centrer la carte seulement si une localisation est disponible
-        centerMapOnLocation(); // Centrer la carte sur la localisation
-    }
-};
-
-  const handleMarkerPress = (marker) => {
-    markerPressedRef.current = true;
-    setSelectedMarker(marker);
-
-    mapRef.current.animateToRegion({
-      latitude: marker.coordinate.latitude - 0.0005,
-      longitude: marker.coordinate.longitude,
-      latitudeDelta: 0.005,
-      longitudeDelta: 0.005,
-    }, 1000);
-
-    if (!isModalVisible) {
-      setIsModalVisible(true);
+  
+      if (!isModalVisible) {
+        setIsModalVisible(true);
+        Animated.timing(animatedHeight, {
+          toValue: Dimensions.get('window').height / 2,
+          duration: 300,
+          useNativeDriver: false,
+        }).start();
+      }
+    };
+  
+    const closeModal = () => {
       Animated.timing(animatedHeight, {
-        toValue: Dimensions.get('window').height / 2,
+        toValue: 0,
         duration: 300,
         useNativeDriver: false,
-      }).start();
-    }
-  };
+      }).start(() => {
+        setIsModalVisible(false);
+        setSelectedMarker(null);
+      });
+    };
+  
+    const handleMapPress = () => {
+      if (!markerPressedRef.current) {
+        closeModal();
+      }
+      markerPressedRef.current = false;
+    };
 
-  const closeModal = () => {
-    Animated.timing(animatedHeight, {
-      toValue: 0,
-      duration: 300,
-      useNativeDriver: false,
-    }).start(() => {
-      setIsModalVisible(false);
-      setSelectedMarker(null);
-    });
-  };
+    
+      const CustomCluster = ({ count }) => (
+        <View style={{
+          width: 50,
+          height: 50,
+          borderRadius: 25,
+          backgroundColor: theme.blue,
+          justifyContent: 'center',
+          alignItems: 'center',
+          shadowColor: '#000',
+            shadowOffset: { width: 0, height: 2 },
+            shadowOpacity: 0.8,
+        }}>
+          <Text style={{ color: 'white', fontFamily:"Inter-Black",fontSize : 20 }}>{count}</Text>
+        </View>
+      );
+  
+    return (
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <MapView
+          ref={mapRef}
+          style={{ flex: 1 }}
+          initialRegion={{
+            latitude: storedLocation?.latitude || DEFAULT_LOCATION.latitude,
+            longitude: storedLocation?.longitude || DEFAULT_LOCATION.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          onPress={handleMapPress}
+          showsUserLocation={true}
+          renderCluster={(cluster) => {
+            const { id, geometry, properties } = cluster;
+            const points = properties.point_count;
 
-  const handleMapPress = () => {
-    if (!markerPressedRef.current) {
-      closeModal();
-    }
-    markerPressedRef.current = false;
-  };
-
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <MapView
-        ref={mapRef}
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: storedLocation?.latitude || DEFAULT_LOCATION.latitude,
-          longitude: storedLocation?.longitude || DEFAULT_LOCATION.longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
-        }}
-        onPress={handleMapPress}
-        showsUserLocation={true}
-        mapType='terrain'
-      >
-        {markersData.map((marker) => (
-          <CustomMarker
-            key={marker.id}
-            marker={marker}
-            handleMarkerPress={handleMarkerPress}
-            markerPressedRef={markerPressedRef}
+            return (
+              <Marker
+                key={`cluster-${id}`}
+                coordinate={{
+                  longitude: geometry.coordinates[0],
+                  latitude: geometry.coordinates[1]
+                }}
+                zIndex={2}
+                onPress={() => console.log("Cluster pressed")}
+              >
+                <CustomCluster count={points} />
+              </Marker>
+            );
+          }}
+        >
+          {markersData.map((marker) => (
+            <Marker
+              key={marker.id}
+              coordinate={marker.coordinate}
+              onPress={() => handleMarkerPress(marker)}
+              zIndex={2}
+            >
+              <View style={{ width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}>
+                <Image 
+                  source={require('./assets/images/marker.png')}
+                  style={{ width: 40, height: 40, position: 'absolute' }}
+                />
+                <Image
+                  source={images[marker.image]}
+                  style={{ width: 25, height: 25, position: 'absolute', top: -2 }}
+                />
+              </View>
+            </Marker>
+          ))}
+        </MapView>
+  
+        {selectedMarker && (
+          <ModalMarker
+            selectedMarker={selectedMarker}
+            closeModal={closeModal}
+            animatedHeight={animatedHeight}
           />
-        ))}
-      </MapView>
-
-      {selectedMarker && (
-        <ModalMarker selectedMarker={selectedMarker} closeModal={closeModal} animatedHeight={animatedHeight} />
-      )}
-
-      {/* Bouton de localisation en bas à droite */}
-      <View style={{
-        position: 'absolute',
-        bottom: 20,
-        right: 20,
-        backgroundColor: 'white',
-        borderRadius: 50,
-        padding: 0,
-        width: 60,
-        height: 60,
-        zIndex: 1,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        elevation: 5,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-        
-      <TouchableOpacity
-        onPress={ () => {}}
-        
-      >
-       
-            
-          <FontAwesome6 name="plus" size={35} color="black" />
-        
-      </TouchableOpacity>
-      </View>
-      <View style={{
-        position: 'absolute',
-        bottom: 90,
-        right: 20,
-        backgroundColor: 'white',
-        borderRadius: 50,
-        padding: 15,
-        width: 60,
-        height: 60,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        elevation: 5,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <TouchableOpacity
-        onPress={handleLocationButtonPress}
-        
-      >
-        {loading ? (
-          <ActivityIndicator size="small" color="black" />
-        ) : error ? (
-          <Ionicons name="alert" size={30} color="red" />
-        ) : (
-            
-          <FontAwesome6 name="location-crosshairs" size={30} color="black" />
         )}
-      </TouchableOpacity>
-      </View>
-    </GestureHandlerRootView>
-  );
-};
+  
+        <View style={{
+          position: 'absolute',
+          bottom: 20,
+          right: 20,
+          backgroundColor: 'white',
+          borderRadius: 50,
+          padding: 0,
+          width: 60,
+          height: 60,
+          zIndex: 1,
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.8,
+          shadowRadius: 2,
+          elevation: 5,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+          <TouchableOpacity onPress={handleLocationButtonPress}>
+            {loading ? (
+              <ActivityIndicator size="small" color="black" />
+            ) : error ? (
+              <Ionicons name="alert" size={30} color="red" />
+            ) : (
+              <FontAwesome6 name="location-crosshairs" size={30} color="black" />
+            )}
+          </TouchableOpacity>
+        </View>
+    
+  
+        {selectedMarker && (
+          <ModalMarker
+            selectedMarker={selectedMarker}
+            closeModal={closeModal}
+            animatedHeight={animatedHeight}
+          />
+        )}
+  
+        {/* Bouton de localisation */}
+        <View style={{
+          position: 'absolute',
+          bottom: 20,
+          right: 20,
+          backgroundColor: 'white',
+          borderRadius: 50,
+          padding: 0,
+          width: 60,
+          height: 60,
+          zIndex: 1,
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.8,
+          shadowRadius: 2,
+          elevation: 5,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+          <TouchableOpacity onPress={handleLocationButtonPress}>
+            {loading ? (
+              <ActivityIndicator size="small" color="black" />
+            ) : error ? (
+              <Ionicons name="alert" size={30} color="red" />
+            ) : (
+              <FontAwesome6 name="location-crosshairs" size={30} color="black" />
+            )}
+          </TouchableOpacity>
+        </View>
+      </GestureHandlerRootView>
+    );
+  };
+  
 
 const images = {
   maison: require('./assets/images/logo_maison.png'),
@@ -536,7 +549,7 @@ const ModalMarker = ({ selectedMarker, closeModal, animatedHeight }) => {
     );
   };
 
-const CustomMarker = ({ marker, handleMarkerPress, markerPressedRef }) => {
+const CustomMarker = ({ marker, handleMarkerPress }) => {
   const image = images[marker.image];
 
   return (
